1e4154358d5e80616fbf00d685e02fffe97109d5
perlancar (@pc-home) <perlancar@gmail.com>
2015-01-03 16:00:59 +0700

    [dist] Move todo items to my todo.org

--- lib/Text/Chart.pm
+++ lib/Text/Chart.pm
@@ -335,31 +335,6 @@ like this beforehand:
  binmode(STDOUT, ":utf8");
 
 
-=head1 TODO
-
-=over
-
-=item * More chart types
-
-=item * Colors
-
-=item * Resampling
-
-Reduce data points, for example I have 1000 numbers that I want to display in a
-80-column chart or sparkline.
-
-=item * Formatting of data (or label)
-
-Using L<Data::Unixish> (like in L<Text::ANSITable>), and specifiable from an
-environment variable.
-
-=item * Option to switch column/row (like in Excel)?
-
-Probably not. I prefer that the data is adjusted instead.
-
-=back
-
-
 =head1 SEE ALSO
 
 L<Text::Graph>, a mature CPAN module for doing text-based graphs. Before writing
